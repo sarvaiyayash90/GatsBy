fbf2aaea735d5ce333a7e4919dafa95f619d9ed6
deep <deepkokje@gmail.com>
2020-04-15 09:11:19 +0530

    final commit 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,14 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from '../components/layout'
 import Head from '../components/head'
+import blogStyles from './blog.module.scss'
 
 const AboutPage =()=>{
     return(
         <div>              
                  <Head title="About"/>
                <Layout>
-            <h2>My name is Deep im from vadodara</h2>
+            <h2>Im from Vadodara</h2>
             <h2>I am a frontend developer</h2>
+            <h2>You can view my projects <a href = "https://github.com/deepkokje" target="_bl"><span className={blogStyles.index}>here</span></a></h2>
+
 
             <p><Link to="/contact">Contact me</Link> </p>
             </Layout>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ const ContactPage =()=>{
                                 <Head title="Contact"/>
                     <Layout>
             <h1>Contact</h1>
-            <p>My name is deep im from vadodara</p>
+            <p>Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard</p>
             <p>Contact me on my <a href = "https://www.linkedin.com/in/deep-kokje-b47b35132/" target="_bl">Linkdin</a></p>
 
             </Layout>
